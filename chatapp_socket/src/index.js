ac79be4449fecbe570b89ebbea696dd8ba13e21c
sanjeevi7777 <104307183+sanjeevi7777@users.noreply.github.com>
2023-12-29 10:53:06 +0530

    FINAL

--- chatapp_socket/src/index.js
+++ chatapp_socket/src/index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://192.168.255.161:3000"],
+    origin: ["http://localhost:3000", "http://192.168.255.161:3000","https://chathubpro.netlify.app"],
     methods: ["GET", "POST"],
   },
 });
